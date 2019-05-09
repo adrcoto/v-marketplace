43a42a659f36e7273db616ead69bbd9e6b850bb3
Adrian Coto <adrcoto@yahoo.com>
2019-05-09 21:02:48 +0300

    Updated Items Layout

--- src/components/item/Items.vue
+++ src/components/item/Items.vue
@@ -1,82 +1,89 @@
 <!--v-bind="{ [`xs${card.flex}`]: true }"-->
 <template>
-    <v-layout row justify-center>
-        <v-flex xs3 sm2 class="mx-3">
-            <v-card>
-                <v-layout column justify-center>
-                    <v-layout row>
-                        <v-card-title>
-                            Filtreaza rezultatele
-                        </v-card-title>
-                    </v-layout>
-                    <v-card-text>
-                        Pret
-                    </v-card-text>
-                    <v-layout row>
-                        <v-card-text>
+    <v-layout justify-center row>
+        <!--                <v-flex sm2 class="ml-3">-->
+        <!--                    <v-card>-->
+        <!--                        <v-layout column justify-center>-->
+        <!--                            <v-layout row>-->
+        <!--                                <v-card-title>-->
+        <!--                                    Filtreaza rezultatele-->
+        <!--                                </v-card-title>-->
+        <!--                            </v-layout>-->
+        <!--                            <v-card-text>-->
+        <!--                                Pret-->
+        <!--                            </v-card-text>-->
+        <!--                            <v-layout row>-->
+        <!--                                <v-card-text>-->
 
-                            <v-text-field placeholder="Pret minim" type="number">
-                            </v-text-field>
-                        </v-card-text>
-                        <v-card-text>
-                            <v-text-field placeholder="Pret maxim" type="number">
-                            </v-text-field>
-                        </v-card-text>
-                    </v-layout>
-                </v-layout>
-            </v-card>
-        </v-flex>
-        <v-flex xs9 sm11>
+        <!--                                    <v-text-field placeholder="Pret minim" type="number">-->
+        <!--                                    </v-text-field>-->
+        <!--                                </v-card-text>-->
+        <!--                                <v-card-text>-->
+        <!--                                    <v-text-field placeholder="Pret maxim" type="number">-->
+        <!--                                    </v-text-field>-->
+        <!--                                </v-card-text>-->
+        <!--                            </v-layout>-->
+        <!--                        </v-layout>-->
+        <!--                    </v-card>-->
+        <!--                </v-flex>-->
+        <v-flex sm8 md12 xl11>
             <v-card>
-                <v-container fluid grid-list-md>
+                <v-container>
                     <v-layout row wrap>
-                        <v-flex v-for="card in cards" :key="card.title" xs12 sm6 md3>
-                            <v-hover>
-                                <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 12 : 2}`" class="mx-1 mb-3">
+                        <v-flex v-for="card in cards" :key="card.title" xs12 sm6 md4 lg3 xl2>
+                            <v-layout justify-center>
+                                <v-hover>
+                                    <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 12 : 2}`" width="92%"
+                                            height="95%"
+                                            class="item-card mb-4">
+                                        <v-img :src="card.src" height="185">
+                                            <v-expand-transition>
+                                                <div v-if="hover"
+                                                     class="d-flex transition-fast-in-fast-out v-card--reveal display-3">
+                                                    <v-card-actions>
+                                                        <v-btn icon>
+                                                            <v-icon color="white">favorite</v-icon>
+                                                        </v-btn>
+                                                        <v-btn icon>
+                                                            <v-icon color="white">bookmark</v-icon>
+                                                        </v-btn>
+                                                        <v-btn icon>
+                                                            <v-icon color="white">share</v-icon>
+                                                        </v-btn>
+                                                        <v-spacer></v-spacer>
+                                                    </v-card-actions>
+                                                </div>
+                                            </v-expand-transition>
+                                        </v-img>
 
-                                    <v-img :src="card.src" height="250px">
-                                        <v-expand-transition>
-                                            <div v-if="hover"
-                                                 class="d-flex transition-fast-in-fast-out v-card--reveal display-3">
-                                                <v-card-actions>
-                                                    <v-btn icon>
-                                                        <v-icon color="white">favorite</v-icon>
-                                                    </v-btn>
-                                                    <v-btn icon>
-                                                        <v-icon color="white">bookmark</v-icon>
-                                                    </v-btn>
-                                                    <v-btn icon>
-                                                        <v-icon color="white">share</v-icon>
-                                                    </v-btn>
-                                                    <v-spacer></v-spacer>
-                                                </v-card-actions>
-                                            </div>
-                                        </v-expand-transition>
-                                    </v-img>
-
-                                    <v-card-title primary-title class="item-card-title">
-                                        <div>
-                                            <div class="subheading font-weight-bold">{{card.title}}
-                                            </div>
-                                        </div>
-                                    </v-card-title>
-                                    <v-chip color="primary" text-color="white" class="item-card-price">
-                                        <span>PRET</span>
-                                        <span class="mx-1">2500</span>
-                                        <v-icon>attach_money</v-icon>
-
-                                    </v-chip>
-                                    <v-card-actions class="item-card-action">
-                                        <v-icon class="grey--text location-icon">location_on</v-icon>
-                                        {{card.location}}
-                                        <v-spacer/>
-                                        <v-btn icon @click="show = !show" class="arrow-icon">
-                                            <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-                                        </v-btn>
-                                    </v-card-actions>
-
-                                </v-card>
-                            </v-hover>
+                                        <v-card-title primary-title class="item-card-title subheading font-weight-bold">
+                                            {{card.title}}
+                                        </v-card-title>
+                                        <v-card-text>
+                                            <v-chip color="primary" text-color="white" class="item-card-price">
+                                                <span>PRET</span>
+                                                <span class="mx-1">2500</span>
+                                                <v-icon>attach_money</v-icon>
+                                            </v-chip>
+                                        </v-card-text>
+                                        <v-card-actions class="item-card-action">
+                                            <span v-if="hover">
+                                                <v-icon class="grey--text">location_on</v-icon>
+                                                {{card.location}}
+                                            </span>
+                                            <span v-else>
+                                                <v-icon class="gray--text">query_builder</v-icon>
+                                                {{card.date}}
+                                            </span>
+                                            <v-spacer/>
+                                            <v-btn icon @click="show = !show" class="arrow-icon">
+                                                <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}
+                                                </v-icon>
+                                            </v-btn>
+                                        </v-card-actions>
+                                    </v-card>
+                                </v-hover>
+                            </v-layout>
                         </v-flex>
                     </v-layout>
                 </v-container>
@@ -98,44 +105,110 @@
                     title: 'Buton reglare lumini Vw Scirocco 2008-prezent Cod: 1K8941333',
                     src: 'https://media2.lajumate.ro/media/i/new_details/5/100/10049795_opel-astra-caravan_5.jpg',
                     location: 'Bucuresti, Ilfov',
-                    flex: 3,
+                    date: '23.05.2019',
                 },
                 {
                     title: 'Favorite road trips',
                     src: 'https://media2.lajumate.ro/media/i/new_details/5/100/10049795_opel-astra-caravan_2.jpg',
                     location: 'Craiova, Dolj',
-                    flex: 3,
+                    date: '02.05.2019',
                 },
                 {
                     title: 'Favorite road trips',
                     src: 'https://media1.lajumate.ro/media/i/new_details/6/100/10042046_iphone-8-64-gb-space-grey_1.jpg',
                     location: 'Bals, Olt',
-                    flex: 3,
+                    date: 'Acum o ora',
                 },
                 {
                     title: 'Favorite road trips',
                     src: 'https://media1.lajumate.ro/media/i/new_details/8/896/8964358_husa-plastic-slim-perforata-huawei-mate-10-10-pro-10-lite_2.jpg',
                     location: 'Bucuresti, Ilfov',
-                    flex: 3,
+                    date: '02.05.2019',
                 },
-                {title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3},
-                {title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3},
-                {title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3},
-                {title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3},
-                {title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3},
+                {
+                    title: 'Best airlines',
+                    src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
+                    location: 'Bucuresti, Ilfov',
+                    date: '02.05.2019',
+                },
+                {
+                    title: 'Favorite road trips',
+                    src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+                    location: 'Bucuresti, Ilfov',
+                    date: '02.05.2019',
+                },
+                {
+                    title: 'Favorite road trips',
+                    src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+                    location: 'Bucuresti, Ilfov',
+                    date: '02.05.2019',
+                },
+                {
+                    title: 'Favorite road trips',
+                    src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+                    location: 'Bucuresti, Ilfov',
+                    date: '02.05.2019',
+                },
+                {
+                    title: 'Favorite road trips',
+                    src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+                    location: 'Bucuresti, Ilfov',
+                    date: '02.05.2019',
+                },
                 {
                     title: 'Far Skoda Fabia 2 stanga - dreapta de la 2007 pana la 2010',
                     src: 'https://media1.lajumate.ro/media/i/new_details/8/987/9879088_far-skoda-fabia-2-stanga-dreapta-de-la-2007-pana-la-2010_1.jpg',
-                    flex: 3,
+                    location: 'Bucuresti, Ilfov',
+                    date: '02.05.2019',
                 },
-                {title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3},
-                {title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3},
-                {title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3},
-                {title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3},
-                {title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3},
-                {title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3},
-                {title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3},
-                {title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3},
+                {
+                    title: 'Favorite road trips',
+                    src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+                    location: 'Bucuresti, Ilfov',
+                    date: '02.05.2019',
+                },
+                {
+                    title: 'Favorite road trips',
+                    src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+                    location: 'Bucuresti, Ilfov',
+                    date: '02.05.2019',
+                },
+                {
+                    title: 'Favorite road trips',
+                    src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+                    location: 'Bucuresti, Ilfov',
+                    date: '02.05.2019',
+                },
+                {
+                    title: 'Favorite road trips',
+                    src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+                    location: 'Bucuresti, Ilfov',
+                    date: '02.05.2019',
+                },
+                {
+                    title: 'Best airlines',
+                    src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
+                    location: 'Bucuresti, Ilfov',
+                    date: '02.05.2019',
+                },
+                {
+                    title: 'Favorite road trips',
+                    src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+                    location: 'Bucuresti, Ilfov',
+                    date: '02.05.2019',
+                },
+                {
+                    title: 'Favorite road trips',
+                    src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+                    location: 'Bucuresti, Ilfov',
+                    date: '02.05.2019',
+                },
+                {
+                    title: 'Favorite road trips',
+                    src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+                    location: 'Bucuresti, Ilfov',
+                    date: '02.05.2019',
+                },
 
             ],
         }),
@@ -144,15 +217,19 @@
 
 <style scoped>
 
-    .item-card-price {
-        position: absolute;
-        top: 165px;
-        right: 10px;
-        height: 30px;
-        padding: 0 6px;
-        line-height: 30px;
-        font-size: 18px;
-        opacity: .8;
+    /*.item-card-price {*/
+    /*    !*top: 165px;*!*/
+    /*    !*right: 10px;*!*/
+    /*   right: 100px;*/
+    /*    height: 30px;*/
+    /*    padding: 0 6px;*/
+    /*    line-height: 30px;*/
+    /*    font-size: 18px;*/
+    /*    opacity: .8;*/
+    /*}*/
+    .item-card-title {
+        height: 100px;
+        overflow: hidden;
     }
 
     /*.item-card-title {*/
